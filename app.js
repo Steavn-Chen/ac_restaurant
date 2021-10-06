7c40a6245c3f61a9bcc397e61d8843c0640d69a6
Steavn Chen <tom9876555@yahoo.com.tw>
2021-10-07 01:11:18 +0800

    新增編輯頁

--- app.js
+++ app.js
@@ -22,12 +22,16 @@ db.once('open', () => {
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
-app.use(express.static("public"));
-app.use(bodyParser.urlencoded({ extend: true}))
 
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true}))
 
 app.get("/", (req, res) => {
-  res.render("index", { restaurants: restaurantList.results });
+  Restaurant.find()
+  .lean()
+  .then(restaurants => res.render('index', { restaurants }))
+  .catch(error => console.log(error))
+  // res.render("index", { restaurants: restaurantList.results });
 });
 
 app.get('/restaurants/new', (req, res) => {
@@ -42,23 +46,86 @@ app.post('/restaurants/new', (req, res) => {
 })
 
 app.get("/search", (req, res) => {
-  const keyword = req.query.keyword;
-  const restaurants = restaurantList.results.filter(function (item) {
-    return (
-      item.name.toLowerCase().includes(keyword.toLowerCase()) ||
-      item.category.toLowerCase().includes(keyword.toLowerCase())
-    );
-  });
-  res.render("index", { restaurants: restaurants, keyword: keyword });
+  const keyword = req.query.keyword.trim()
+  console.log(keyword)
+  // 第一種方法
+  // const regex = new RegExp( keyword.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&"), 'gi')
+  // console.log(regex,typeof regex)
+  // Restaurant.find({$or:[{name: regex},{category: regex}]})
+  // .lean()
+  // .then(restaurants => { if (restaurants.length) return res.render('index', { restaurants ,keyword}) 
+  //   return res.render('searchOut',{ keyword})
+  // })
+  // .catch(error => console.log(error))  
+
+// 第二種mongoose語法
+   Restaurant.find({$or:[{ name:{$regex: `${keyword}`, $options: 'i' } },{ category:{$regex: `${keyword}`, $options: 'i' } }]})
+  .lean()
+  .then(restaurants => { if (restaurants.length) return res.render('index', { restaurants ,keyword}) 
+    return res.render('searchOut',{ keyword})
+  })
+  .catch(error => console.log(error)) 
+  // const restaurants = restaurantList.results.filter(function (item) {
+  //   return item.name.toLowerCase().includes(keyword.toLowerCase()) ||
+  //     item.category.toLowerCase().includes(keyword.toLowerCase())
+  // });
+  // res.render("index", { restaurants: restaurants, keyword: keyword });
 });
 
 app.get("/restaurants/:restaurant_id", (req, res) => {
-  const restaurant_id = req.params.restaurant_id;
-  const restaurant = restaurantList.results.find((item) => {
-    return item.id.toString() === restaurant_id;
-  });
-  res.render("show", { restaurant: restaurant });
+  const id = req.params.restaurant_id;
+  Restaurant.findById(id)
+  .lean()
+  .then(restaurant => res.render('show',{restaurant}))
+  .catch(error => console.log(error))
+   
+  // const restaurant = restaurantList.results.find((item) => {
+  //   return item.id.toString() === restaurant_id;
+  // });
+  // res.render("show", { restaurant: restaurant });
 });
+
+app.get("/restaurants/:restaurant_id/edit", (req, res) => {
+  const id = req.params.restaurant_id;
+  Restaurant.findById(id)
+  .lean()
+  .then(restaurant => res.render('edit',{restaurant}))
+  .catch(error => console.log(error))
+   
+  // const restaurant = restaurantList.results.find((item) => {
+  //   return item.id.toString() === restaurant_id;
+  // });
+  // res.render("show", { restaurant: restaurant });
+});
+
+app.post("/restaurants/:restaurant_id/edit", (req, res) => {
+  const id = req.params.restaurant_id;
+  const body = req.body
+  // console.log(body)
+  const { name, name_en, category, image, location, phone, google_map, rating, description } = req.body
+  return Restaurant.findById(id)
+  // .lean()
+  .then(restaurant => {
+    restaurant.name = name,
+    restaurant.name_en = name_en,
+    restaurant.category = category,
+    restaurant.image = image,
+    restaurant.location = location,
+    restaurant.phone = phone,
+    restaurant.google_map = google_map,
+    restaurant.rating = rating,
+    restaurant.description = description
+    return restaurant.save()
+    // console.log(restaurant.name)
+    // res.render('edit',{ restaurant })
+  })
+  .then(restaurant => {
+    // console.log(restaurant)
+    res.redirect(`/restaurants/${id}/edit`)
+  })
+  .catch(error => console.log(error))
+});
+
 app.listen(port, () => {
   console.log(`Express is running on http://locahost:${port}`);
 });
