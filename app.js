1a7fb83f19488fa503a7f0b46278d6663c24e4db
Steavn Chen <tom9876555@yahoo.com.tw>
2021-10-09 21:54:46 +0800

    refactor: modify express routes

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const express = require("express");
 const exphbs = require("express-handlebars");
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
-const restaurantList = require("./restaurant.json");
-const Restaurant = require('./models/restaurant')
+const routes = require('./routes/index')
+// const restaurantList = require("./restaurant.json");
+// const Restaurant = require('./models/restaurant')
 const mongoose = require('mongoose')
 const app = express();
 
@@ -29,116 +30,7 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true}))
 app.use(methodOverride('_method'))
 
-app.get("/", (req, res) => {
-  Restaurant.find()
-  .lean()
-  .sort({ _id: 'asc'})
-  .then(restaurants => res.render('index', { restaurants }))
-  .catch(error => console.log(error))
-  // res.render("index", { restaurants: restaurantList.results });
-});
-
-app.get('/restaurants/new', (req, res) => {
-  res.render('new')
-})
-
-app.post('/restaurants/new', (req, res) => {
-  const newData = req.body
-  Restaurant.create(newData)
-  res.redirect('/')
-  // res.render('new')
-})
-
-app.get("/search", (req, res) => {
-  const keyword = req.query.keyword.trim()
-  console.log(keyword)
-  // 第一種方法
-  // const regex = new RegExp( keyword.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&"), 'gi')
-  // console.log(regex,typeof regex)
-  // Restaurant.find({$or:[{name: regex},{category: regex}]})
-  // .lean()
-  // .then(restaurants => { if (restaurants.length) return res.render('index', { restaurants ,keyword}) 
-  //   return res.render('searchOut',{ keyword})
-  // })
-  // .catch(error => console.log(error))  
-
-// 第二種mongoose語法
-   Restaurant.find({$or:[{ name:{$regex: `${keyword}`, $options: 'i' } },{ category:{$regex: `${keyword}`, $options: 'i' } }]})
-  .lean()
-  .then(restaurants => { if (restaurants.length) return res.render('index', { restaurants ,keyword}) 
-    return res.render('searchOut',{ keyword})
-  })
-  .catch(error => console.log(error)) 
-  // const restaurants = restaurantList.results.filter(function (item) {
-  //   return item.name.toLowerCase().includes(keyword.toLowerCase()) ||
-  //     item.category.toLowerCase().includes(keyword.toLowerCase())
-  // });
-  // res.render("index", { restaurants: restaurants, keyword: keyword });
-});
-
-app.get("/restaurants/:restaurant_id", (req, res) => {
-  const id = req.params.restaurant_id;
-  Restaurant.findById(id)
-  .lean()
-  .then(restaurant => res.render('show',{restaurant}))
-  .catch(error => console.log(error))
-   
-  // const restaurant = restaurantList.results.find((item) => {
-  //   return item.id.toString() === restaurant_id;
-  // });
-  // res.render("show", { restaurant: restaurant });
-});
-
-app.get("/restaurants/:restaurant_id/edit", (req, res) => {
-  const id = req.params.restaurant_id;
-  Restaurant.findById(id)
-  .lean()
-  .then(restaurant => res.render('edit',{restaurant}))
-  .catch(error => console.log(error))
-   
-  // const restaurant = restaurantList.results.find((item) => {
-  //   return item.id.toString() === restaurant_id;
-  // });
-  // res.render("show", { restaurant: restaurant });
-});
-
-app.put("/restaurants/:restaurant_id", (req, res) => {
-  const id = req.params.restaurant_id;
-  const body = req.body
-
-  // 第一種寫法解購函式
-  // const { name, name_en, category, image, location, phone, google_map, rating, description } = req.body
-  // return Restaurant.findById(id)
-  // .then(restaurant => {
-  //   restaurant.name = name,
-  //   restaurant.name_en = name_en,
-  //   restaurant.category = category,
-  //   restaurant.image = image,
-  //   restaurant.location = location,
-  //   restaurant.phone = phone,
-  //   restaurant.google_map = google_map,
-  //   restaurant.rating = rating,
-  //   restaurant.description = description
-  //   return restaurant.save() 
-  // })
-
-  // 第二種物件合併
-  return Restaurant.findById(id)
-  .then(restaurant=> { 
-      Object.assign(restaurant,body).save() 
-  })
-  .then(()=> res.redirect(`/restaurants/${id}/edit`))
-  .catch(error => console.log(error))
-});
-
-app.delete('/restaurants/:restaurant_id', (req, res)=> {
-  const id = req.params.restaurant_id
-  console.log(id)
-  Restaurant.findById(id)
-  .then(restaurant => restaurant.remove())
-  .then(() => res.redirect('/'))
-  .catch(error => console.log(error)) 
-})
+app.use(routes)
 
 app.listen(port, () => {
   console.log(`Express is running on http://locahost:${port}`);
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,1 +1,16 @@
-const
+const express = require('express')
+const router = express.Router()
+
+const Restaurant = require('../../models/restaurant')
+
+router.get("/", (req, res) => {
+  Restaurant.find()
+  .lean()
+  .sort({ _id: 'asc'})
+  .then(restaurants => res.render('index', { restaurants }))
+  .catch(error => console.log(error))
+  // res.render("index", { restaurants: restaurantList.results });
+});
+
+
+module.exports = router
