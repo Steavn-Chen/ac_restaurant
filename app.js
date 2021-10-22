29723d99c5a1cc3eac804a5168c81c0cc17ecf46
Steavn Chen <tom9876555@yahoo.com.tw>
2021-10-22 21:08:23 +0800

    express-session

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-const express = require("express");
+const express = require('express');
+const session = require('express-session')
 const exphbs = require("express-handlebars");
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
@@ -12,9 +13,14 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 app.use(express.static("public"));
-
 app.use(bodyParser.urlencoded({ extended: true}))
 app.use(methodOverride('_method'))
+
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 
 app.use(routes)
 
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../../models/user')
 
 router.get('/login', (req, res) => {
   res.render('login')
 })
 
 router.post('/login', (req, res) => {
+  
   res.render('index')
 })
 
@@ -14,7 +16,23 @@ router.get('/register', (req, res) => {
 })
 
 router.post('/register', (req, res) => {
-  res.render('login')
+  const { name, email, password, confirmPassword } = req.body
+  console.log(name, email, password, confirmPassword)
+  User.findOne({ email })
+  .lean()
+  .then(user => { 
+    if (user) { 
+      console.log('此帳號己經註冊過了')
+      return res.render('register', { name, email, password, confirmPassword })
+    }
+    return User.create({
+      name,
+      email,
+      password
+    })
+    .then(() => res.redirect('/'))
+    .catch(err => console.log(err))
+  })
 })
 
 module.exports = router
