7a05c9015d290ee760f91a435251e4d2ce70282b
Steavn Chen <tom9876555@yahoo.com.tw>
2021-10-22 15:53:56 +0800

    feat: add register page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -9,4 +9,12 @@ router.post('/login', (req, res) => {
   res.render('index')
 })
 
+router.get('/register', (req, res) => {
+  res.render('register')
+})
+
+router.post('/register', (req, res) => {
+  res.render('login')
+})
+
 module.exports = router
